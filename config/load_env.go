208c3db1e95d01da5e8d0ca8566f84c34b278b94
Ulises Farfan <ulisesfarfan296@gmail.com>
2023-11-13 20:34:12 -0300

    fix

--- config/load_env.go
+++ config/load_env.go
@@ -19,15 +19,9 @@ type Config struct {
 }
 
 func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.AutomaticEnv()
-	viper.SetConfigType("env")
 	viper.SetConfigFile(".env")
-
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
-	}
+	viper.ReadInConfig()
+	viper.AutomaticEnv()
 
 	new_config := Config{
 		DbUrl:          viper.GetString("MONGO_URL"),
